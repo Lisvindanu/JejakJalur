b456d64ab2099258a84c87b4ebf9dadb8d96d596
Lyramor <mmarsa2435@gmail.com>
2026-05-20 02:46:43 +0700

    feat: update project structure and enhance UI components
    - Renamed project from "JejakJalur" to "jejakjalur-fe" in package-lock.json and package.json.
    - Added @tabler/icons-react dependency for improved icon usage.
    - Enhanced CSS with new font import and animations for a smoother user experience.
    - Updated welcome page with mock data for destinations and cities, including new UI components for displaying ratings and card content.

--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -1,1 +1,12 @@
 export type * from './auth';
+
+export type Flash = {
+    sukses?: string;
+    error?: string;
+};
+
+export type SharedProps = {
+    auth: import('./auth').Auth;
+    flash: Flash;
+    name: string;
+};
